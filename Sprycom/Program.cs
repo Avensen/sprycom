2a9e9953edb91dac7b923fef445d1b694554b1b1
Avensen Nejersian <anejersian@gmail.com>
2021-11-15 14:38:35 -0500

    Added some missing configuration files

---
+++ Sprycom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sprycom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
